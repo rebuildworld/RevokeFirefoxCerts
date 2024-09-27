0e1221ef7e5789595d58f96c137291a3741dce8d
rebuildworld <22651937+rebuildworld@users.noreply.github.com>
2024-09-27 11:24:11 +0800

    吊销Tor Browser的证书

---
+++ torbrowser.sh
@@ -0,0 +1,15 @@
+
+firefox_dir="C:/Users/${USERNAME}/Desktop/Tor Browser"
+#firefox_dir="D:/Tool/Browser/Tor Browser"
+
+profile_dir="${firefox_dir}/Browser/TorBrowser/Data/Browser/profile.default"
+echo $profile_dir
+
+for file in ./certs/*
+do
+  nickname=$(openssl x509 -noout -subject -in "$file" | sed -n '/^subject/s/^.*CN=//p')
+  certutil -D -d sql:"$profile_dir" -n "$nickname"
+  certutil -A -d sql:"$profile_dir" -n "$nickname" -i "$file" -t "p,p,p"
+done
+
+certutil -L -d sql:"$profile_dir"
